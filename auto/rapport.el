3a34b347f36083bbb151c7dd2baba4b871e7bdc6
Luigi Franco Tedesco <tedesco.luigi@gmail.com>
2015-08-17 14:42:42 +0200

    monday 14h45

--- auto/rapport.el
+++ auto/rapport.el
@@ -1,21 +1,19 @@
 (TeX-add-style-hook
  "rapport"
  (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("report" "a4paper" "notitlepage" "twoside" "10pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "english" "french") ("inputenc" "utf8") ("gnuplottex" "miktex") ("algpseudocode" "noend") ("geometry" "a4paper" "total={6in,9in}")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "environments"
@@ -23,23 +21,25 @@
     "empty"
     "fancyhf"
     "descriptif"
-    "etatdelart"
     "approche"
     "resultats"
     "conclusion"
     "bibliographie"
+    "presentationThales"
+    "annexe"
     "report"
     "rep10"
     "babel"
     "inputenc"
     "rotating"
     "amsmath"
+    "gensymb"
     "amsfonts"
-    "graphicx"
     "latexsym"
     "amssymb"
     "probsoln"
     "enumerate"
+    "tikz"
     "amsthm"
     "fancyhdr"
     "multirow"
--- rapport.d
+++ rapport.d
@@ -1,1 +1,1 @@
-rapport.dvi rapport.d : rapport.tex environments.tex cover.tex empty.tex fancyhf.tex descriptif.tex etatdelart.tex approche.tex resultats.tex conclusion.tex bibliographie.tex    
+rapport.dvi rapport.d : rapport.tex environments.tex cover.tex empty.tex fancyhf.tex descriptif.tex approche.tex resultats.tex conclusion.tex bibliographie.tex annexe.tex   enstalogo.png upmclogo.png pfh_frame.png pfh_diagram.png fpfh_diagram.png second_component.jpg object_model.png chair_db.jpg box_seq.png chair_db.jpg chair_seq.png chair4w.png  
